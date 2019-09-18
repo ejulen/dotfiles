f24126991eec0fdbeaf9b2815842833c7e921759
Johannes Ulén <johannes@lagenhetsbyte.se>
2019-09-18 09:13:07 +0200

    Removed OMF.

--- omf/.config/omf/before.init.fish
+++
@@ -1,7 +0,0 @@
-set -gx VIRTUAL_ENV_DISABLE_PROMPT 1
-
-set -gx FZF_DEFAULT_COMMAND "rg --files --vimgrep"
-
-set -g theme_color_scheme terminal-light
-set -g theme_nerd_fonts yes
-set -g theme_newline_cursor yes
